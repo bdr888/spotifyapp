c829cae02d5a37c7f0262953b790d3516d66a238
briandridge <briandridge@gmail.com>
2016-12-13 16:47:07 -0700

    got routes working

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,22 @@
-console.log('linked main.js');
 
 // get request  - search for artist, get the id of that artist, get that artist's related artists
 $(document).ready(function(){
 	$("#getRelatedArtists").on("submit", function(){
 		event.preventDefault();
+		// for Each
 		var searchArtist = $("#searchArtist").val();
-		console.log(searchArtist);
-		$.get('https://api.spotify.com/v1/search?q=' + searchArtist + '&type=artist').done(function(response) {
-			var resObj = response;
-			console.log(resObj);
-			response.parseJSON(body);
-			console.log("response?"+response);
+		$.get('https://api.spotify.com/v1/search?q=' + searchArtist + '&type=artist').done(function(req, res) {
+			var name = req.artists.items[0].name;
+			var image = req.artists.items[0].images[3].url;
+			console.log(req);
+			$("#results").append("<li>" + name + "</li>");
+			// console.log("response?"+response);
 			// get artist id
-			$.get('https://api.spotify.com/v1/artists/' + resObj.artists.items.id + '/related-artists').done(function(response) {
-				// var $sentence = jQuery.parseJSON($sentenceResults.responseText);
-			$("#results").append("<li>" + response.artists.items.id + "</li>");
+			// $.get('https://api.spotify.com/v1/artists/' + resObj.artists.items.id + '/related-artists').done(function(response) {
+			// 	// var $sentence = jQuery.parseJSON($sentenceResults.responseText);
+			// $("#results").append("<li>" + response.artists.items.id + "</li>");
 		});
-		});
+	});
 
 	// 	var $results = $.get('https://api.spotify.com/v1/artists/'+ $searchArtist + '3kSqc2brwAF1kWRFWYe2fW/related-artists').done(function(response){
 	// 		// var $sentence = jQuery.parseJSON($sentenceResults.responseText);
@@ -25,10 +25,8 @@ $(document).ready(function(){
 	// 		// push res back end db
 	// 		// $.post to bakend
 	// 	});
-	});
+	// });
 });
-
-
 
 
 
